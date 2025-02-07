c531c6796fe7169864b329060e960226e04e2392
Junjiecheng2024 <c15919822199@gmail.com>
2025-02-07 13:51:22 +0200

    updated

--- task_manager/task.py
+++ task_manager/task.py
@@ -1,11 +1,7 @@
 class Task:
-    def __init__(self, title, description="", completed=False):
+    def __init__(self, title, description=""):
         self.title = title
         self.description = description
-        self.completed = completed
-
-    def mark_completed(self):
-        self.completed = True
 
     def __repr__(self):
-        return f"Task(title='{self.title}', description='{self.description}', completed={self.completed})"
+        return f"Task(title='{self.title}', description='{self.description}')"
